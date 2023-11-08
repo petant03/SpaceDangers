9780a7254cbbb07cc5ccb80a85015b312419c608
petant03 <matteo.antonini03@gmail.com>
2023-11-08 22:35:02 +0100

    Rename class

--- Assets/Scripts/BottomBar.cs
+++ Assets/Scripts/BottomBar.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class HomeMenu : MonoBehaviour
+public class BottomBar : MonoBehaviour
 {
     public void StartGame()
     {
